858b853e8f90d8546d7febe8cb18866f07f7c1f3
eminatutic <eminatutic73@gmail.com>
2026-03-14 11:04:02 +0100

    iniy

--- obj/Release/net8.0/FlexFit.AssemblyInfo.cs
+++ obj/Release/net8.0/FlexFit.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlexFit")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f0b7938fea913af7c4b2bb028bd910c20ff6c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlexFit")]
